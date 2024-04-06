81d39fdb5fb8d6be8a12bdd42614f7e5bbd032e1
cr1pt0_d3v <cr1pt0.d3v@gmail.com>
2024-04-06 19:33:15 +0200

    create new routes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,46 @@
-import { Flex, Highlight, Spacer } from '@chakra-ui/react';
+import { Flex, Spacer, Text, Link as ExternalLink } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
 import RoutesComponent from './RoutesComponent';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import BuyLembuButton from './BuyLembuButton';
 
 const Navbar = () => {
   return (
-    <Flex minWidth="max-content" alignItems="center" gap="2" py={6}>
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      p={4}
+      bg="gray.900"
+    >
       <Link to={'/'}>
-        <Highlight
-          query="$LEMBU"
-          styles={{ px: '2', py: '1', rounded: 'full', bg: 'red.100' }}
+        <Text
+          bgGradient="linear(to-r, #7928CA, #FF0080)"
+          bgClip="text"
+          fontSize="2xl"
+          fontWeight="extrabold"
         >
           $LEMBU
-        </Highlight>
+        </Text>
       </Link>
       <Flex>
         <RoutesComponent />
       </Flex>
+      <ExternalLink
+        _hover={{ textDecoration: 'none' }}
+        href="https://dexscreener.com/base/0xb147ffbf8163238e6b8987963ae31b4381f2454e"
+        isExternal
+      >
+        <Text
+          color="gray.50"
+          fontSize="xl"
+          fontWeight="extrabold"
+          _hover={{ color: '#FF0080' }}
+        >
+          DexScreener
+        </Text>
+      </ExternalLink>
+      <BuyLembuButton />
       <Spacer />
       <ConnectButton />
     </Flex>
