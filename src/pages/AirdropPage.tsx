40de8a2aaa2fd8c10501d8f56faa1b8f6fa37a02
cr1pt0_d3v <cr1pt0.d3v@gmail.com>
2024-04-06 19:31:57 +0200

    create airdrop page

---
+++ src/pages/AirdropPage.tsx
@@ -0,0 +1,13 @@
+import { Box, Center, Heading } from '@chakra-ui/react';
+
+const AirdropPage = () => {
+  return (
+    <Box minH="80vh" bg="gray.900">
+      <Center>
+        <Heading color="gray.50">Comming soon...</Heading>
+      </Center>
+    </Box>
+  );
+};
+
+export default AirdropPage;
