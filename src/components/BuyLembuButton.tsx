a6f4a209c203a714c4f55b0cfe2b64b3d778439d
cr1pt0_d3v <cr1pt0.d3v@gmail.com>
2024-04-06 19:40:03 +0200

    fix the side effects of the hover

--- src/components/BuyLembuButton.tsx
+++ src/components/BuyLembuButton.tsx
@@ -3,6 +3,8 @@ import { Button, Link, Text } from '@chakra-ui/react';
 const BuyLembuButton = () => {
   return (
     <Button
+      bgGradient="gray.50"
+      color="#FF0080"
       _hover={{
         bgGradient: 'linear(to-b, #7928CA, #FF0080)',
         color: 'gray.50',
@@ -14,12 +16,7 @@ const BuyLembuButton = () => {
         href="https://app.uniswap.org/swap?outputCurrency=0x1f3D1d87Ba637201b0006672E9bB98800e33B670&chain=base"
         isExternal
       >
-        <Text
-          color="#FF0080"
-          fontSize="xl"
-          fontWeight="extrabold"
-          _hover={{ color: 'gray.50' }}
-        >
+        <Text fontSize="xl" fontWeight="extrabold">
           BUY $LEMBU
         </Text>
       </Link>
