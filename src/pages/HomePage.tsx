b48a7ff73b1d07ce1c8100a38f98ef5a82ed7b7e
cr1pt0_d3v <cr1pt0.d3v@gmail.com>
2024-04-07 21:17:43 +0200

    create the homepage and background image

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,24 @@
-import { Box } from '@chakra-ui/react';
+import { AbsoluteCenter, Box, Card, Heading, Text } from '@chakra-ui/react';
 
 const HomePage = () => {
-  return <Box minH="80vh" bg="gray.900"></Box>;
+  return (
+    <Box
+      minH="80vh"
+      bgImage="./assets/lambo-hd.jpg"
+      bgPosition="center"
+      bgRepeat="no-repeat"
+      bgSize="cover"
+    >
+      <AbsoluteCenter>
+        <Heading color="gray.50" textAlign="center">
+          🚗 Welcome to $LEMBU 🚗
+        </Heading>
+        <Heading size="md" color="gray.50">
+          🎉 Join the fastest meme coin revolution in the BASE chain! 🎉
+        </Heading>
+      </AbsoluteCenter>
+    </Box>
+  );
 };
 
 export default HomePage;
