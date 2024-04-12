17a4deaf5e440faf51a067a540de6bfc58465178
cr1pt0_d3v <cr1pt0.d3v@gmail.com>
2024-04-12 21:50:25 +0200

    refactor the main app

--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,22 @@ import { Buffer } from 'buffer';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { MainStore } from "./store/main-store";
-import { StoreProvider } from "./store/store-provider";
+import { MainStore } from './store/main-store';
+import { StoreProvider } from './store/store-provider';
 
 import './index.css';
 
-
 import '@rainbow-me/rainbowkit/styles.css';
-import Main2 from './main2.tsx';
-
+import MainApp from './MainApp.tsx';
 
 globalThis.Buffer = Buffer;
-
 
 const mainStore = new MainStore();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <StoreProvider value={mainStore}>
-    <Main2/>
+    <StoreProvider value={mainStore}>
+      <MainApp />
     </StoreProvider>
   </React.StrictMode>,
 );
